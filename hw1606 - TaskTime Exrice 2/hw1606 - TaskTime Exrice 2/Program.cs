e9a0605f1c9c208e6edc2759b17f14abef7e294d
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-06-19 17:55:37 +0300

    Add files via upload

---
+++ hw1606 - TaskTime Exrice 2/hw1606 - TaskTime Exrice 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw1606___TaskTime_Exrice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            Task t1 = new Task<int>(() =>
+            {
+                int time = r.Next(1000, 2000);
+                Thread.Sleep(time);
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId} Wait: {time} miliseconds!");
+                return time;
+            });
+
+            Task t2 = new Task<int>(() =>
+            {
+                int time = r.Next(1000, 2000);
+                Thread.Sleep(time);
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId} Wait: {time} miliseconds!");
+                return time;
+            });
+            t1.Start();
+            t2.Start();
+
+            //*****  Wait For All Tasks.
+            Task.WaitAll(t1, t2);
+            Console.WriteLine("All Threads As Been Done");
+
+            //*****  Wait Just For One Thread.
+            Task.WaitAny(t1, t2);
+            Console.WriteLine("One Task Is Over");
+        }
+    }
+}
---
+++ hw1606- Task(DemoConnectToDB) Exrice 1/hw1606- Task(DemoConnectToDB) Exrice 1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw1606__Task_DemoConnectToDB__Exrice_1
+{
+    //All Threads Get In The Gate Until I Close Him!
+    //class Program
+    //{
+    //    static ManualResetEvent mre = new ManualResetEvent(false);
+    //    static private void CreateCustomer()
+    //    {
+    //        mre.WaitOne();
+    //        Console.WriteLine("Customer Created!");
+    //        Thread.Sleep(1000);
+
+    //    }
+    //    static private void RemoveOrder()
+    //    {
+    //        mre.WaitOne();
+    //        Console.WriteLine("Your Order Removed");
+    //        Thread.Sleep(1000);
+
+    //    }
+    //    static private void ConnectToDB()
+    //    {
+    //        Console.WriteLine("Connection Succesfull!");
+    //        Thread.Sleep(3000);
+    //        mre.Set();
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        new Task(CreateCustomer).Start();
+    //        new Task(RemoveOrder).Start();
+    //        new Task(ConnectToDB).Start();
+    //
+    //        while (true)
+    //        {
+    //
+    //        }
+    //    }
+
+
+    //Just One Thread Get In The Gate And Close The Gate!
+    class Program
+    {
+        static AutoResetEvent are = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            new Task(() =>
+            {
+                are.WaitOne();
+                Console.WriteLine("Customer Created!");
+                Thread.Sleep(1000);
+                are.Set();
+            }).Start();
+            new Task(() =>
+            {
+                are.WaitOne();
+                Console.WriteLine("Your Order Removed");
+                Thread.Sleep(1000);
+                are.Set();
+            }).Start();
+            new Task(() =>
+            {
+                Console.WriteLine("Connection Succesfull!");
+                Thread.Sleep(3000);
+                are.Set();
+            }).Start();
+    
+            while (true)
+            {
+    
+            }
+        }
+}
+}
